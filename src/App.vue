c1680cdf30e65809eda99203042056b3b87e992b
B W <36702082+Bwilliamson55@users.noreply.github.com>
2022-03-05 13:58:35 -0500

    Add Dashboard component skeleton, Add Icons, Name Nav Links

--- src/App.vue
+++ src/App.vue
@@ -23,28 +23,42 @@ export default {
   <router-view v-slot="{Component}">
     <TransitionRoot as="template" :show="sidebarOpen">
       <Dialog as="div" class="fixed inset-0 flex z-40 md:hidden" @close="sidebarOpen = false">
-        <TransitionChild as="template" enter="transition-opacity ease-linear duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="transition-opacity ease-linear duration-300" leave-from="opacity-100" leave-to="opacity-0">
-          <DialogOverlay class="fixed inset-0 bg-gray-600 bg-opacity-75" />
+        <TransitionChild as="template" enter="transition-opacity ease-linear duration-300" enter-from="opacity-0"
+                         enter-to="opacity-100" leave="transition-opacity ease-linear duration-300"
+                         leave-from="opacity-100" leave-to="opacity-0">
+          <DialogOverlay class="fixed inset-0 bg-gray-600 bg-opacity-75"/>
         </TransitionChild>
-        <TransitionChild as="template" enter="transition ease-in-out duration-300 transform" enter-from="-translate-x-full" enter-to="translate-x-0" leave="transition ease-in-out duration-300 transform" leave-from="translate-x-0" leave-to="-translate-x-full">
+        <TransitionChild as="template" enter="transition ease-in-out duration-300 transform"
+                         enter-from="-translate-x-full" enter-to="translate-x-0"
+                         leave="transition ease-in-out duration-300 transform" leave-from="translate-x-0"
+                         leave-to="-translate-x-full">
           <div class="relative flex-1 flex flex-col max-w-xs w-full pt-5 pb-4 bg-gray-800">
-            <TransitionChild as="template" enter="ease-in-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in-out duration-300" leave-from="opacity-100" leave-to="opacity-0">
+            <TransitionChild as="template" enter="ease-in-out duration-300" enter-from="opacity-0"
+                             enter-to="opacity-100" leave="ease-in-out duration-300" leave-from="opacity-100"
+                             leave-to="opacity-0">
               <div class="absolute top-0 right-0 -mr-12 pt-2">
-                <button type="button" class="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" @click="sidebarOpen = false">
+                <button type="button"
+                        class="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                        @click="sidebarOpen = false">
                   <span class="sr-only">Close sidebar</span>
-                  <XIcon class="h-6 w-6 text-white" aria-hidden="true" />
+                  <XIcon class="h-6 w-6 text-white" aria-hidden="true"/>
                 </button>
               </div>
             </TransitionChild>
             <div class="flex-shrink-0 flex items-center px-4">
-              <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow" />
+              <img class="h-8 w-auto"
+                   src="/blackjack.svg" alt="Cards"/>
+              <span class="text-white text-xl font-large px-2 py-2">CardVue</span>
             </div>
             <div class="mt-5 flex-1 h-0 overflow-y-auto">
               <nav class="px-2 space-y-1">
-                <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'group flex items-center px-2 py-2 text-base font-medium rounded-md']">
-                  <component :is="item.icon" :class="[item.current ? 'text-gray-300' : 'text-gray-400 group-hover:text-gray-300', 'mr-4 flex-shrink-0 h-6 w-6']" aria-hidden="true" />
+                <router-link v-for="item in navigation" :key="item.name" :to="item.href"
+                   :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'group flex items-center px-2 py-2 text-base font-medium rounded-md']">
+                  <component :is="item.icon"
+                             :class="[item.current ? 'text-gray-300' : 'text-gray-400 group-hover:text-gray-300', 'mr-4 flex-shrink-0 h-6 w-6']"
+                             aria-hidden="true"/>
                   {{ item.name }}
-                </a>
+                </router-link>
               </nav>
             </div>
           </div>
@@ -56,27 +70,34 @@ export default {
     </TransitionRoot>
 
     <!-- Static sidebar for desktop -->
-    <div class="hidden md:flex md:w-64 md:flex-col md:fixed md:inset-y-0">
+    <div class="hidden md:flex md:w-44 md:flex-col md:fixed md:inset-y-0">
       <!-- Sidebar component, swap this element with another sidebar if you like -->
       <div class="flex-1 flex flex-col min-h-0 bg-gray-800">
         <div class="flex items-center h-16 flex-shrink-0 px-4 bg-gray-900">
-          <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow" />
+          <img class="h-8 w-auto" src="/blackjack.svg"
+               alt="Cards"/>
+          <span class="bg-gray-900 text-white text-xl font-large px-2 py-2">CardVue</span>
         </div>
         <div class="flex-1 flex flex-col overflow-y-auto">
           <nav class="flex-1 px-2 py-4 space-y-1">
-            <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'group flex items-center px-2 py-2 text-sm font-medium rounded-md']">
-              <component :is="item.icon" :class="[item.current ? 'text-gray-300' : 'text-gray-400 group-hover:text-gray-300', 'mr-3 flex-shrink-0 h-6 w-6']" aria-hidden="true" />
+            <router-link v-for="item in navigation" :key="item.name" :to="item.href"
+               :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'group flex items-center px-2 py-2 text-sm font-medium rounded-md']">
+              <component :is="item.icon"
+                         :class="[item.current ? 'text-gray-300' : 'text-gray-400 group-hover:text-gray-300', 'mr-3 flex-shrink-0 h-6 w-6']"
+                         aria-hidden="true"/>
               {{ item.name }}
-            </a>
+            </router-link>
           </nav>
         </div>
       </div>
     </div>
-    <div class="md:pl-64 flex flex-col">
+    <div class="md:pl-44 flex flex-col">
       <div class="sticky top-0 z-10 flex-shrink-0 flex h-16 bg-white shadow">
-        <button type="button" class="px-4 border-r border-gray-200 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden" @click="sidebarOpen = true">
+        <button type="button"
+                class="px-4 border-r border-gray-200 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden"
+                @click="sidebarOpen = true">
           <span class="sr-only">Open sidebar</span>
-          <MenuAlt2Icon class="h-6 w-6" aria-hidden="true" />
+          <MenuAlt2Icon class="h-6 w-6" aria-hidden="true"/>
         </button>
         <div class="flex-1 px-4 flex justify-between">
           <div class="flex-1 flex">
@@ -84,30 +105,45 @@ export default {
               <label for="search-field" class="sr-only">Search</label>
               <div class="relative w-full text-gray-400 focus-within:text-gray-600">
                 <div class="absolute inset-y-0 left-0 flex items-center pointer-events-none">
-                  <SearchIcon class="h-5 w-5" aria-hidden="true" />
+                  <SearchIcon class="h-5 w-5" aria-hidden="true"/>
                 </div>
-                <input id="search-field" class="block w-full h-full pl-8 pr-3 py-2 border-transparent text-gray-900 placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-0 focus:border-transparent sm:text-sm" placeholder="Search" type="search" name="search" />
+                <input id="search-field"
+                       class="block w-full h-full pl-8 pr-3 py-2 border-transparent text-gray-900 placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-0 focus:border-transparent sm:text-sm"
+                       placeholder="Search" type="search" name="search"/>
               </div>
             </form>
           </div>
           <div class="ml-4 flex items-center md:ml-6">
-            <button type="button" class="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            <button type="button"
+                    class="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
               <span class="sr-only">View notifications</span>
-              <BellIcon class="h-6 w-6" aria-hidden="true" />
+              <BellIcon class="h-6 w-6" aria-hidden="true"/>
             </button>
 
             <!-- Profile dropdown -->
             <Menu as="div" class="ml-3 relative">
               <div>
-                <MenuButton class="max-w-xs bg-white flex items-center text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                <MenuButton
+                    class="max-w-xs bg-white flex items-center text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                   <span class="sr-only">Open user menu</span>
-                  <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                  <img class="h-8 w-8 rounded-full"
+                       src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                       alt=""/>
                 </MenuButton>
               </div>
-              <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
-                <MenuItems class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+              <transition enter-active-class="transition ease-out duration-100"
+                          enter-from-class="transform opacity-0 scale-95"
+                          enter-to-class="transform opacity-100 scale-100"
+                          leave-active-class="transition ease-in duration-75"
+                          leave-from-class="transform opacity-100 scale-100"
+                          leave-to-class="transform opacity-0 scale-95">
+                <MenuItems
+                    class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
                   <MenuItem v-for="item in userNavigation" :key="item.name" v-slot="{ active }">
-                    <a :href="item.href" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">{{ item.name }}</a>
+                    <a :href="item.href"
+                       :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">{{
+                        item.name
+                      }}</a>
                   </MenuItem>
                 </MenuItems>
               </transition>
@@ -118,13 +154,16 @@ export default {
 
       <main class="flex-1">
         <div class="py-6">
-          <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+          <div class="max-w-7xl max-h-max mx-auto px-4 sm:px-6 md:px-8 justify-items-start flex">
             <h1 class="text-2xl font-semibold text-gray-900">Dashboard</h1>
+            <PresentationChartBarIcon class="h-max w-8 ml-2" aria-hidden="true"/>
           </div>
           <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
             <!-- Replace with your content -->
             <div class="py-4">
-              <div class="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+              <div class="rounded-lg h-auto">
+                <TheDashboard />
+              </div>
             </div>
             <!-- /End replace -->
           </div>
@@ -135,7 +174,7 @@ export default {
 </template>
 
 <script>
-import { ref } from 'vue'
+import {ref} from 'vue'
 import {
   Dialog,
   DialogOverlay,
@@ -147,30 +186,34 @@ import {
   TransitionRoot,
 } from '@headlessui/vue'
 import {
+  AdjustmentsIcon,
   BellIcon,
   CalendarIcon,
   ChartBarIcon,
+  CollectionIcon,
   FolderIcon,
   HomeIcon,
   InboxIcon,
   MenuAlt2Icon,
+  PresentationChartBarIcon,
+  TableIcon,
   UsersIcon,
   XIcon,
 } from '@heroicons/vue/outline'
-import { SearchIcon } from '@heroicons/vue/solid'
+import {SearchIcon} from '@heroicons/vue/solid'
+import TheDashboard from '@/components/TheDashboard.vue'
 
 const navigation = [
-  { name: 'Dashboard', href: '#', icon: HomeIcon, current: true },
-  { name: 'Team', href: '#', icon: UsersIcon, current: false },
-  { name: 'Projects', href: '#', icon: FolderIcon, current: false },
-  { name: 'Calendar', href: '#', icon: CalendarIcon, current: false },
-  { name: 'Documents', href: '#', icon: InboxIcon, current: false },
-  { name: 'Reports', href: '#', icon: ChartBarIcon, current: false },
+  {name: 'Dashboard', href: '/', icon: HomeIcon, current: true},
+  {name: 'Cards', href: '#', icon: CollectionIcon, current: false},
+  {name: 'Decks', href: '#', icon: TableIcon, current: false},
+  {name: 'Teams', href: '#', icon: UsersIcon, current: false},
+  {name: 'Settings', href: '#', icon: AdjustmentsIcon, current: false},
 ]
 const userNavigation = [
-  { name: 'Your Profile', href: '#' },
-  { name: 'Settings', href: '#' },
-  { name: 'Sign out', href: '#' },
+  {name: 'Your Profile', href: '#'},
+  {name: 'Settings', href: '#'},
+  {name: 'Sign out', href: '#'},
 ]
 
 export default {
@@ -183,9 +226,15 @@ export default {
     MenuItems,
     TransitionChild,
     TransitionRoot,
+    AdjustmentsIcon,
     BellIcon,
+    CollectionIcon,
     MenuAlt2Icon,
+    PresentationChartBarIcon,
     SearchIcon,
+    TheDashboard,
+    TableIcon,
+    UsersIcon,
     XIcon,
   },
   setup() {
---
+++ src/components/TheDashboard.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <h3 class="text-lg leading-6 font-medium text-gray-900">Some Stats!</h3>
+    <dl class="mt-5 grid grid-cols-1 rounded-lg bg-white overflow-hidden shadow divide-y divide-gray-200 md:grid-cols-3 md:divide-y-0 md:divide-x">
+      <div v-for="item in stats" :key="item.name" class="px-4 py-5 sm:p-6">
+        <dt class="text-base font-normal text-gray-900">
+          {{ item.name }}
+        </dt>
+        <dd class="mt-1 flex justify-between items-baseline md:block lg:flex">
+          <div class="flex items-baseline text-2xl font-semibold text-indigo-600">
+            {{ item.stat }}
+            <span class="ml-2 text-sm font-medium text-gray-500"> from {{ item.previousStat }} </span>
+          </div>
+
+          <div :class="[item.changeType === 'increase' ? 'bg-green-100 text-green-800' : 'bg-red-100 text-red-800', 'inline-flex items-baseline px-2.5 py-0.5 rounded-full text-sm font-medium md:mt-2 lg:mt-0']">
+            <ArrowSmUpIcon v-if="item.changeType === 'increase'" class="-ml-1 mr-0.5 flex-shrink-0 self-center h-5 w-5 text-green-500" aria-hidden="true" />
+            <ArrowSmDownIcon v-else class="-ml-1 mr-0.5 flex-shrink-0 self-center h-5 w-5 text-red-500" aria-hidden="true" />
+            <span class="sr-only"> {{ item.changeType === 'increase' ? 'Increased' : 'Decreased' }} by </span>
+            {{ item.change }}
+          </div>
+        </dd>
+      </div>
+    </dl>
+  </div>
+  <hr class="border-2 my-3"/>
+  <div>
+    <h2 class="text-gray-500 text-xs font-medium uppercase tracking-wide">Quick Actions</h2>
+    <ul role="list" class="mt-3 grid grid-cols-1 gap-5 sm:gap-6 sm:grid-cols-2 lg:grid-cols-4">
+      <li v-for="project in projects" :key="project.name" class="col-span-1 flex shadow-sm rounded-md">
+        <div :class="[project.bgColor, 'flex-shrink-0 flex items-center justify-center w-16 text-white text-sm font-medium rounded-l-md']">
+          {{ project.initials }}
+        </div>
+        <div class="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
+          <div class="flex-1 px-4 py-2 text-sm truncate">
+            <a :href="project.href" class="text-gray-900 font-medium hover:text-gray-600">{{ project.name }}</a>
+            <p class="text-gray-500">{{ project.members }} Members</p>
+          </div>
+          <div class="flex-shrink-0 pr-2">
+            <button type="button" class="w-8 h-8 bg-white inline-flex items-center justify-center text-gray-400 rounded-full bg-transparent hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              <span class="sr-only">Open options</span>
+              <DotsVerticalIcon class="w-5 h-5" aria-hidden="true" />
+            </button>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+
+</template>
+
+<script>
+import { ArrowSmDownIcon, ArrowSmUpIcon, DotsVerticalIcon } from '@heroicons/vue/solid'
+
+const stats = [
+  { name: 'Highest Damage Deck', stat: '71,897', previousStat: '70,946', change: '12%', changeType: 'increase' },
+  { name: 'Avg. Deck Damage', stat: '58.16%', previousStat: '56.14%', change: '2.02%', changeType: 'increase' },
+  { name: 'Worst Deck', stat: '24.57%', previousStat: '28.62%', change: '4.05%', changeType: 'decrease' },
+]
+const projects = [
+  { name: 'Create Card', initials: 'CC', href: '#', members: 16, bgColor: 'bg-pink-600' },
+  { name: 'Create Deck', initials: 'CD', href: '#', members: 12, bgColor: 'bg-purple-600' },
+  { name: 'Create Team', initials: 'T', href: '#', members: 16, bgColor: 'bg-yellow-500' },
+  { name: 'Stat View', initials: 'SV', href: '#', members: 8, bgColor: 'bg-green-500' },
+]
+
+export default {
+  components: {
+    ArrowSmDownIcon,
+    ArrowSmUpIcon,
+    DotsVerticalIcon,
+  },
+  setup() {
+    return {
+      stats,
+      projects,
+    }
+  },
+}
+</script>
